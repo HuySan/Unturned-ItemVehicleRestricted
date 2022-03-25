f822c0076cf29082e816cbb59a133a5e1b706a57
IdiosGame <36539661+HuySan@users.noreply.github.com>
2022-03-25 16:47:33 +0300

    Add plugin

---
+++ carPermission/Configuration.cs
@@ -0,0 +1,44 @@
+﻿using Rocket.API;
+using System.Collections.Generic;
+using carPermission.Types;
+
+namespace vehiclePermission
+{
+    public class Configuration : IRocketPluginConfiguration
+    {
+        public List<Settings> CanSeatOnTheTransport;
+        public void LoadDefaults()
+        {
+            CanSeatOnTheTransport = new List<Settings>
+            {
+                new Settings
+                {
+                    idGroup = "default",
+                    WhiteList = new List<int>
+                    {
+                        1,
+                        123,
+                    }
+                },
+                new Settings
+                {
+                    idGroup = "police",
+                    WhiteList = new List<int>
+                    {
+                        33,
+                        106,
+                        108,
+                    }
+                },
+                new Settings
+                {
+                    idGroup = "fireman",
+                    WhiteList = new List<int>
+                    {
+                        34,
+                    }
+                },
+            };
+        }
+    }
+}
---
+++ carPermission/Types/Settings.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace carPermission.Types
+{
+    public class Settings
+    {
+        [XmlAttribute]
+        public string idGroup;
+        [XmlArray]
+        public List<int> WhiteList;
+
+        public bool IsInWhiteList(int idVeh)
+        {
+            foreach(int id in WhiteList)
+            {
+                if (id == idVeh) return true;
+
+            }
+            return false;
+        }
+    }
+}
